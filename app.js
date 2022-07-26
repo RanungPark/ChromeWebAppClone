18c66789af181245bec25ddd726d9be202d20c82
RanungPark <ckehfqkr302@naver.com>
2022-07-27 01:41:01 +0900

    Recap

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
-function plus(firstNumber, secondNumber){
-  console.log(firstNumber + secondNumber);
-}
-plus(7,5);
+const calculator = {
+  add : function(firstNume, secondNume){
+    console.log(firstNume + secondNume);
+  },
+  minus  : function(firstNume, secondNume){
+    console.log(firstNume - secondNume);
+  },
+  divide : function(firstNume, secondNume){
+    console.log(firstNume / secondNume);
+  },
+  powerof : function(firstNume, secondNume){
+    console.log(firstNume ** secondNume);
+  },
+};
+
+calculator.add(10,5);
+calculator.minus(10,5);
+calculator.divide(10,5);
+calculator.powerof(10,5);
