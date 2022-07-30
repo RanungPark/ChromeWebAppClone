5380fada0b5c406772e25a094059d618ac8c02b9
RanungPark <ckehfqkr302@naver.com>
2022-07-31 00:08:36 +0900

    22-07-31

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
-const age = parseInt( prompt("How old are you?"));
+const hellos = document.getElementsByClassName('hello');
 
-if (isNaN(age) || age < 0){
-  console.log("Please write a real postitve number");
-}else if(age < 18){
-  console.log("You are too young");
-}else if(age >= 18 && age <= 50){
-  console.log("You can drink");
-}else if(age > 50 && age <= 80){
-  console.log("You should exercise");
-}else if(age > 80){
-  console.log("You can do what ever you want");
-}
+hellos[1].innerHTML("Got you!");
--- learn.md
+++ learn.md
@@ -8,3 +8,8 @@ const a = 5;
 consol.log
 parseInt를 하면 숫자인지 알수 있고 범위를 알수 있다
 isNaN 불리언으로 return 해준다 숫자인지 아닌지 알려준다 숫자면 false 그외의 것이면 true
+
+- document는 html을 가지고 온다 (객체이다)
+- document.getElementById를 통하여 ID값이 가리키는 요소를 가지고 올수있다
+- .dir은 전체 요소를 출력한다
+- document.querySelector는 element 요소를 css방식으로 검색할수 있다
